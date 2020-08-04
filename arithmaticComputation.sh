66674be2a510ef865ccbc2093d2db984e26bbb5b
pranav8553 <pranavCIC065@gmail.com>
2020-08-04 14:23:02 +0530

    Added: Computed the forth expression 'a % b + c'

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -2,6 +2,8 @@
 
 echo -e "WELCOME TO ARITHMATIC COMPUTATION AND SORTING PROBLEM"
 
+declare -A Computations
+
 read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
@@ -9,3 +11,4 @@ read -p "Enter the value of c: " c
 result1=`awk "BEGIN {print $a + $b * $c}"`
 result2=`awk "BEGIN {print $a * $b + $c}"`
 result3=`awk "BEGIN {print $c + $a / $b}"`
+result4=`awk "BEGIN {print $a % $b + $c}"`
