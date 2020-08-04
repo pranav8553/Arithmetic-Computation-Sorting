5de1a7be93b8c9e4cb2494e706b52067fe1e09c1
pranav8553 <pranavCIC065@gmail.com>
2020-08-04 13:31:14 +0530

    Added: Computed the first expression 'a + b * c'

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -5,3 +5,5 @@ echo -e "WELCOME TO ARITHMATIC COMPUTATION AND SORTING PROBLEM"
 read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
+
+result1=`awk "BEGIN {print $a + $b * $c}"`
